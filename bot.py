2aa81d72f9c92090f366d2a761626fc9eafae7cb
onur880 <turna.onurcan@gmail.com>
2025-07-10 20:55:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ chrome_options.binary_location = "/usr/bin/google-chrome"
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
 
 service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
