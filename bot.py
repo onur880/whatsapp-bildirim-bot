21aabb6d610b74dab6236235d4cc8ad715c64b21
onur880 <turna.onurcan@gmail.com>
2025-07-10 20:26:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/google-chrome"
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--headless")  # BU SATIRI KALDIR
 
 service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
