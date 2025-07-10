f9c68c9e5773b488ccfc94910d156b3ec9b4bda4
onur880 <turna.onurcan@gmail.com>
2025-07-10 21:54:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import time
 
+# Chrome seçenekleri
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_argument("--headless=new")
-chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--headless")  # Ekransız mod
 
-service = Service("/usr/bin/chromedriver")
+service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-driver.get("https://www.google.com")
-print(driver.title)
-driver.quit()
+try:
+    driver.get("https://www.sahibinden.com/")  # Örnek URL, istediğin sayfayı yazabilirsin
+    print(driver.title)
+
+    # Buraya fırsat ilanları kontrol ve mesaj gönderme logic'i eklenir
+    time.sleep(5)
+
+finally:
+    driver.quit()
