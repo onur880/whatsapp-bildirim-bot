0a8324071b14744368252032e894f27a496ff029
onur880 <turna.onurcan@gmail.com>
2025-07-10 20:16:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-# Chrome seçeneklerini ayarla
 chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/google-chrome"
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Chrome driver servisini ayarla
 service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Test için Google'a git
 driver.get("https://www.google.com")
 print(driver.title)
 
