8433928175571b1424dd86c4457494a44e31ee1a
onur880 <turna.onurcan@gmail.com>
2025-07-10 20:22:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,9 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/google-chrome"
-# Headless kapalı, gerçek tarayıcı açacak
-chrome_options.add_argument("--no-sandbox")
+# Headless kapalı, gerçek pencere açacak
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
 
 service = Service('/usr/bin/chromedriver')
