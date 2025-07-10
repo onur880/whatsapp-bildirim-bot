088aeab69c98b2b85e1f2ceffe23af16265caeab
onur880 <turna.onurcan@gmail.com>
2025-07-10 21:46:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,26 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-# Chrome seçenekleri
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_argument("--headless=new")  # <<< EN ÖNEMLİ
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--window-size=1920,1080")
 
-# ChromeDriver servisi
 service = Service("/usr/bin/chromedriver")
-
-# Tarayıcıyı başlat
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Test için Google'a git
 driver.get("https://www.google.com")
-
-# Sayfa başlığını yazdır
 print(driver.title)
-
-# Tarayıcıyı kapat
 driver.quit()
